042e2d0acae88c7d1dd4ebd8616fee900a2a9fb6
Thiago C. S. Tioma <thgsouza@outlook.com>
2023-11-01 13:46:55 -0300

    feat: footer hover opacity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className + " min-h-screen"}>
         {children}
-        <footer className="text-center text-gray-lighter text-sm flex flex-row justify-center items-center w-full fixed bottom-1 md:bottom-2 opacity-50">Developed with ❤️ by <a target='_blank' href="https://github.com/souzathg" className='hover:text-primary ml-1'>@souzathg</a></footer>
+        <footer className="text-center hover:opacity-100 text-gray-lighter text-sm flex flex-row justify-center items-center w-full fixed bottom-1 md:bottom-2 opacity-50">Developed with ❤️ by <a target='_blank' href="https://github.com/souzathg" className='hover:text-primary ml-1'>@souzathg</a></footer>
       </body>
     </html>
   )
