e608eab264eab812dc6f16c8cd86dadc5bb60a9a
Thiago C. S. Tioma <thgsouza@outlook.com>
2023-12-12 08:06:18 -0300

    feat: add google-site-verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head> 
+        <meta name="google-site-verification" content="pQZ1zm7Kmbepityc9mDyUj9nqsXa-P8-du3wcAHtbpc" />
+      </head>
       <body className={inter.className + " min-h-screen"}>
         {children}
         <footer className="text-center hover:opacity-100 text-gray-lighter text-sm flex flex-row justify-center items-center w-full fixed bottom-1 md:bottom-2 opacity-50">Developed with ❤️ by <a target='_blank' href="https://github.com/souzathg" className='hover:text-primary ml-1'>@souzathg</a></footer>
